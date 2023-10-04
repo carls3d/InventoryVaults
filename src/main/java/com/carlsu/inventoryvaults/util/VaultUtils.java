110b401a506777f69b9a8598a6f46a7e60a0aba3
Carlsu~ <104013959+carls3d@users.noreply.github.com>
2023-10-04 22:08:46 +0200

    Update VaultUtils.java

--- src/main/java/com/carlsu/inventoryvaults/util/VaultUtils.java
+++ src/main/java/com/carlsu/inventoryvaults/util/VaultUtils.java
@@ -7,10 +7,16 @@ import javax.annotation.Nullable;
 import com.carlsu.inventoryvaults.types.PlayerData;
 import com.ibm.icu.impl.Pair;
 
+import net.minecraft.core.Registry;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.nbt.TagType;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.server.ServerLifecycleHooks;
 
 public class VaultUtils implements IVaultData{
     
@@ -36,6 +42,11 @@ public class VaultUtils implements IVaultData{
     public static String getVaultsString(Player player, String locationKey) {
         CompoundTag inventoryVaults = getInventoryVaults(player);
         return inventoryVaults.getString(locationKey);
+    }
+    public static String getStringOrDefault(Player player, String location, String defaultValue) {
+        CompoundTag inventoryVaults = getInventoryVaults(player);
+        String vaultsString = inventoryVaults.getString(location);
+        return ifElseValidKey(vaultsString, vaultsString, defaultValue);
     }
     
     public static HashMap<String, Pair<Byte, TagType<?>>> getTagTypes(Player player, @Nullable String locationKey) {
@@ -81,5 +92,25 @@ public class VaultUtils implements IVaultData{
     public static String ifElseValidKey(String key, String ifValid, String ifNotValid) {
         return ifElseString(validKey(key), ifValid, ifNotValid);
     }
-    
+
+
+
+    /* Dimension type getters */
+    public static ServerLevel getServerLevel(String dimension) {
+        return getServerLevel(new ResourceLocation(dimension));
+    }
+    public static ServerLevel getServerLevel(ResourceLocation resourceLocation) {
+        ResourceKey<Level> dimensionKey = getResourceKey(resourceLocation);
+        return getServerLevel(dimensionKey);
+    }
+    public static ServerLevel getServerLevel(ResourceKey<Level> dimensionKey) {
+        return ServerLifecycleHooks.getCurrentServer().getLevel(dimensionKey);
+    }
+
+    public static ResourceKey<Level> getResourceKey(String dimension) {
+        return getResourceKey(new ResourceLocation(dimension));
+    }
+    public static ResourceKey<Level> getResourceKey(ResourceLocation resourceLocation) {
+        return ResourceKey.create(Registry.DIMENSION_REGISTRY, resourceLocation);
+    }
 }
